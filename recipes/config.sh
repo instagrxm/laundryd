240e6b1c64bf58b101c0fcc1301778a35f8b74fa
Josh Santangelo <josh@endquote.com>
2020-02-22 08:47:44 -0800

    work on file arguments

--- recipes/config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# get current directory https://stackoverflow.com/a/246128
-export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-export LAUNDRY_CMD=${DIR}/../bin/run
-export LAUNDRY_FOO=BAR
