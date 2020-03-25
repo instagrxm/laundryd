55576d951361d8772fddef42f379556febe10315
Josh Santangelo <josh@endquote.com>
2020-03-25 16:25:21 -0700

    feedbin liked/starred items

---
+++ src/washers/feedbin/feedbin.ts
@@ -0,0 +1,92 @@
+import { flags } from "@oclif/command";
+import { OutputFlags } from "@oclif/parser/lib/parse";
+import { DateTime } from "luxon";
+import { Item } from "../../core/item";
+import { Log } from "../../core/log";
+import { Shared } from "../../core/washers/shared";
+import { Wash } from "../../core/washers/wash";
+import { Washer } from "../../core/washers/washer";
+
+export class Feedbin {
+  static api = "https://api.feedbin.com/v2";
+
+  // The biggest favicon
+  static icon =
+    "https://assets.feedbin.com/assets/apple-touch-icon-precomposed-74dca373532cc762b726c2f291755fec4e12e27abdd255d99927730dc4430112.png";
+
+  // The URL to the site
+  static url = "https://feedbin.com";
+
+  static authSettings = {
+    username: flags.string({
+      required: true,
+      description: "Feedbin username"
+    }),
+
+    password: flags.string({
+      required: true,
+      description: "Feedbin password"
+    })
+  };
+
+  /**
+   * Check a username/password against the authentication API. This isn't stateful,
+   * you still need to include the auth in every request.
+   * https://github.com/feedbin/feedbin-api/blob/master/content/authentication.md
+   * @param washer the washer making the request
+   * @param auth the auth settings
+   */
+  static async auth(
+    washer: Washer,
+    auth: OutputFlags<typeof Feedbin.authSettings>
+  ): Promise<void> {
+    try {
+      await Shared.queueHttp(washer, undefined, {
+        url: `${Feedbin.api}/authentication.json`,
+        auth: { username: auth.username, password: auth.password }
+      });
+    } catch (error) {
+      await Log.error(washer, { msg: "auth failed", error: error.message });
+    }
+  }
+
+  /**
+   * Convert a raw API object into an Item.
+   * @param data the show object from the API
+   */
+  static async parseData(washer: Wash, data: any): Promise<Item> {
+    // Create an item from the API response
+    const item: Item = {
+      url: data.url,
+      created: DateTime.fromISO(data.published),
+      title: data.title,
+      summary: data.summary,
+      html: data.content
+    };
+
+    if (data.author) {
+      item.author = { name: data.author };
+    }
+
+    // Try to get the extracted content for more info
+    if (data.extracted_content_url) {
+      try {
+        const extract = await Shared.queueHttp(washer, undefined, {
+          url: data.extracted_content_url
+        });
+        item.html = extract.data.content;
+        item.image = extract.data.lead_image_url;
+      } catch (error) {
+        await Log.warn(washer, {
+          msg: "couldn't get extracted content",
+          url: item.url
+        });
+      }
+    }
+
+    // TODO: Get item.text by putting html into phantomjs, then reading .innerText?
+    // https://stackoverflow.com/a/5002618/468472
+
+    return item;
+  }
+}
---
+++ src/washers/feedbin/liked.ts
@@ -0,0 +1,71 @@
+import { OutputFlags } from "@oclif/parser/lib/parse";
+import clone from "clone";
+import { Item } from "../../core/item";
+import { Shared } from "../../core/washers/shared";
+import { Wash } from "../../core/washers/wash";
+import { WasherInfo } from "../../core/washers/washerInfo";
+import { Feedbin } from "./feedbin";
+
+export default class Liked extends Wash {
+  static readonly info = new WasherInfo({
+    title: "Feedbin likes",
+    description: "load posts you've starred on Instagram"
+  });
+
+  static settings = {
+    ...Wash.settings,
+    ...Feedbin.authSettings
+  };
+
+  config!: OutputFlags<typeof Liked.settings>;
+
+  async init(): Promise<void> {
+    await Feedbin.auth(this, this.config);
+  }
+
+  async run(): Promise<Item[]> {
+    // Request the IDs of the starred entries
+    let res = await Shared.queueHttp(this, undefined, {
+      url: `${Feedbin.api}/starred_entries.json`,
+      responseType: "json",
+      auth: { username: this.config.username, password: this.config.password }
+    });
+    const entryIds = res.data as number[];
+
+    // IDs don't come back in a useful order, so save them and check against the list so we don't
+    // request the same ones again.
+    let getEntries = entryIds;
+    if (this.memory.entryIds) {
+      getEntries = entryIds.filter(id => !this.memory.entryIds.includes(id));
+    }
+    this.memory.entryIds = clone(entryIds);
+
+    // Request the contents of the starred entries, 100 at a time
+    let data: any[] = [];
+    while (getEntries.length) {
+      const page = getEntries.splice(0, 100);
+      res = await Shared.queueHttp(this, undefined, {
+        url: `${Feedbin.api}/entries.json`,
+        responseType: "json",
+        params: { ids: page.join(",") },
+        auth: { username: this.config.username, password: this.config.password }
+      });
+      data = data.concat(res.data);
+    }
+
+    // Convert entries to Items
+    return Promise.all(data.map(d => this.parseData(d)));
+  }
+
+  async parseData(data: any): Promise<Item> {
+    const item = await Feedbin.parseData(this, data);
+
+    item.source = {
+      image: Feedbin.icon,
+      url: Feedbin.url,
+      title: this.info.title
+    };
+
+    return item;
+  }
+}
