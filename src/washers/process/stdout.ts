7207aa36af63ced1ee437ee3cd252235c36b6218
Josh Santangelo <josh@endquote.com>
2020-03-31 13:22:03 -0700

    compact option for stdout

--- src/washers/process/stdout.ts
+++ src/washers/process/stdout.ts
@@ -1,6 +1,6 @@
 import { OutputFlags } from "@oclif/parser/lib/parse";
 import clone from "clone";
-import util from "util";
+import util, { InspectOptions } from "util";
 import { Dry, Item, Settings, WasherInfo } from "../../core";
 
 export class Stdout extends Dry {
@@ -16,10 +16,24 @@ export class Stdout extends Dry {
     color: Settings.boolean({
       default: false,
       description: "output in color"
+    }),
+
+    compact: Settings.boolean({
+      default: false,
+      description: "false to output each object to a single line"
     })
   };
 
   config!: OutputFlags<typeof Stdout.settings>;
+  inspectOptions!: InspectOptions;
+
+  async init(): Promise<void> {
+    this.inspectOptions = {
+      colors: this.config.color,
+      compact: this.config.compact,
+      breakLength: Number.POSITIVE_INFINITY
+    };
+  }
 
   async run(items: Item[]): Promise<void> {
     for (const item of items) {
@@ -28,7 +42,8 @@ export class Stdout extends Dry {
         i.saved = item.saved.toJSDate();
       }
       i.created = item.created.toJSDate();
-      const json = util.inspect(i, undefined, undefined, this.config.color);
+
+      const json = util.inspect(i, this.inspectOptions);
       process.stdout.write(json + "\n");
     }
   }
