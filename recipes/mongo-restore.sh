e8c813cdf68713603c69e302d9a109f488b17a1e
Josh Santangelo <josh@endquote.com>
2020-03-31 15:36:37 +0000

    mongorestore works

--- recipes/mongo-restore.sh
+++ recipes/mongo-restore.sh
@@ -3,9 +3,9 @@
 
 # get current directory https://stackoverflow.com/a/246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source ${DIR}/config.sh
+source ${DIR}/config.prod.sh
 
 # https://docs.mongodb.com/manual/reference/program/mongodump/
 
 # replace mongo.dump with the path to the file
-mongorestore --uri=${MONGO_CONN} --drop --archive=mongo.dump
+mongorestore --uri=${MONGO_CONN} --drop --archive=/home/ubuntu/tmp/mongo.dump 
